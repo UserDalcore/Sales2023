15ea05bc0394eeebcd14ef8048da2458acd70e2c
dalcore <contacto@dalcore.es>
2025-10-23 18:26:03 +0200

    Reinicio

---
+++ Sales.Mobile/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Sales.Mobile")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Sales.Mobile.Pages")]
